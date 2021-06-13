732eec121dd9ac3809780b9ec212de4079f8565c
JulioF127 <juliofuentes12@hotmail.com>
2021-06-13 03:05:46 -0600

    Ultimas correciones en el import

--- Dr_Strange/src/main/resources/import.sql
+++ Dr_Strange/src/main/resources/import.sql
@@ -1,3 +1,4 @@
 Insert into Paciente (id, nombre, edad) values (1, 'José Luis Meraz Carbajal', 34);
-
-Insert into Cita (id, paciente, observaciones, estado, fecha, hora) values (1, 'José Luis Meraz Carbajal', 'Presenta sintomas de neumonía', 'ACEPTADO', '08/07/2021', '14:30');
+Insert into Paciente (id, nombre, edad) values (2, 'Adair Quesada Valdés Rincón', 44);
+Insert into Cita (id, paciente, observaciones, estado, fecha, hora) values (1, 'José Luis Meraz Carbajal', 'Presenta sintomas de neumonía', 'ACEPTADO', '08/02/2021', '14:30');
+Insert into Cita (id, paciente, observaciones, estado, fecha, hora) values (2, 'Adair Quesada Valdés Rincón', 'Ingresado por multiples fracturas', 'ACEPTADO', '14/04/2021', '18:00');
